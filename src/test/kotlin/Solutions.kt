26aa863329974395b51c424c4e966a052bebe234
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2022-12-01 10:19:09 +0100

    day 1 added

--- src/test/kotlin/Solutions.kt
+++ src/test/kotlin/Solutions.kt
@@ -5,4 +5,18 @@ class Solutions {
     fun day00test() {
         assert(day00(IO.getInputText(0)))
     }
+
+    @Test
+    fun day01test() {
+        val ans = day01(IO.getInputLines(1))
+        println(ans)
+        assert(ans == 71023)
+    }
+
+    @Test
+    fun day01p2test() {
+        val ans = day01p2(IO.getInputLines(1))
+        println(ans)
+        assert(ans == 206289)
+    }
 }
